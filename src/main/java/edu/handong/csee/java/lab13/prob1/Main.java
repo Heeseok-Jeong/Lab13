12939bde3f8e200af3ab01dd9b7b29ff488d9ddb
Heeseok-Jeong <dldydldy@naver.com>
2018-05-01 01:41:31 +0900

    modify prob1

--- src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
@@ -3,8 +3,13 @@ package edu.handong.csee.java.lab13.prob1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Friend friend1 = new Friend();
+		SchoolFriend friend2 = new SchoolFriend();
+		ClassFriend friend3 = new ClassFriend();
+		
+		InstanceOf.whatFriend(friend1);
+		InstanceOf.whatFriend(friend2);
+		InstanceOf.whatFriend(friend3);
 	}
 
 }

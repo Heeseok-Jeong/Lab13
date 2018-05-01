9de9b7aa52f82beb01f4ad4428fa3ee622540145
Heeseok-Jeong <dldydldy@naver.com>
2018-05-02 02:20:19 +0900

    most complete prob5

--- src/main/java/edu/handong/csee/java/lab13/prob5/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Main.java
@@ -1,10 +1,58 @@
 package edu.handong.csee.java.lab13.prob5;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main extends Prob4{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n1 = 0, n2 = 0;
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter row size(max 5)");
+		n1 = keyboard.nextInt();
+		System.out.print("Enter column size(max 5)");
+		n2 = keyboard.nextInt();
+		int[][] m1 = new int[n1][n2];
+		
+		System.out.print("Enter row size(max 5)");
+		n1 = keyboard.nextInt();
+		System.out.print("Enter column size(max 5)");
+		n2 = keyboard.nextInt();
+		int[][] m2 = new int[n1][n2];
+		
+		System.out.print("Enter List1 : ");
+		for(int i=0; i < m1.length; i++)
+			for(int j=0; j < m2.length; j++)
+				m1[i][j] = keyboard.nextInt();
+		
+		System.out.print("Enter List2 : ");
+		for(int i=0; i < m1.length; i++)
+			for(int j=0; j < m2.length; j++)
+				m2[i][j] = keyboard.nextInt();
+		
+		for(int i=0; i < m1.length; i++) {
+			for(int j=0; j < m2.length; j++)
+				System.out.print(m1[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int i=0; i < m1.length; i++) {
+			for(int j=0; j < m2.length; j++)
+				System.out.print(m2[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println();
+		
+		if(equals(m1, m2))
+			System.out.println("The two arrays are approximately identical.");
+		else
+			System.out.println("The two arrays are not identical.");
+		
+		
+		
+		
+		
 	}
 
 }
--- src/main/java/edu/handong/csee/java/lab13/prob5/Prob4.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Prob4.java
@@ -1,5 +1,20 @@
 package edu.handong.csee.java.lab13.prob5;
 
 public class Prob4 {
-
+	public static boolean equals(int[][] m1, int[][] m2) {
+		int count = 0;
+		if(m1.length != m2.length) 
+			return false; 
+		else {
+			for(int i=0; i < m1.length; i++)
+				for(int j=0; j < m2.length; j++)
+				if (m1[i][j] != m2[i][j])
+					count++;
+		}
+		
+		if(count <= 3)
+			return true;
+		else
+			return false;
+	}
 }

86187d939e565aab3ba79f6ee7168a970b350d54
Heeseok-Jeong <dldydldy@naver.com>
2018-05-02 01:32:14 +0900

    most complete prob3

--- src/main/java/edu/handong/csee/java/lab13/prob4/Master.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Master.java
@@ -1,11 +1,31 @@
 package edu.handong.csee.java.lab13.prob4;
 
-public class Master {
+import java.util.Scanner;
 
+public class Master {
+	public void feed(Pet pet) {
+		System.out.println("feed : " + pet.food());
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String catName, dogName;
+		
+		Master master = new Master();
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter the cat name and dog name : ");
+		catName = keyboard.next();
+		dogName = keyboard.next();
+		
+		cat.setName(catName);
+		cat.getName();
+		master.feed(cat);
+	
+		dog.setName(dogName);
+		dog.getName();
+		master.feed(dog);
 	}
 
 }

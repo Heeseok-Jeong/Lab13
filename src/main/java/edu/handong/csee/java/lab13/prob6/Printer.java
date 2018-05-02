ac1680c1c5daf02ca18d6cf192331fc57d9a01d7
Heeseok-Jeong <dldydldy@naver.com>
2018-05-02 22:39:53 +0900

    complete all programs

--- src/main/java/edu/handong/csee/java/lab13/prob6/Printer.java
+++ src/main/java/edu/handong/csee/java/lab13/prob6/Printer.java
@@ -3,7 +3,7 @@ package edu.handong.csee.java.lab13.prob6;
 public class Printer { //the class Printer
 	public static void print(Object object) { //the static method print has a Object(ultimate superclass) parameter
 		String str = object.toString(); //String type local variable value is each object's toString return value 
-		if(object instanceof CapitalPrintable) //if object is subclass of CapitalPrintable class
+		if(object instanceof CapitalPrintable) //if object follows CapitalPrintable class
 			str = str.toUpperCase(); //str value changes to upper cases
 		System.out.println(str); //print the result
 	}
--- src/main/java/edu/handong/csee/java/lab13/prob6/UpPoint.java
+++ src/main/java/edu/handong/csee/java/lab13/prob6/UpPoint.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab13.prob6;
 
-public class UpPoint { //the class UpPoint
+public class UpPoint implements CapitalPrintable { //the class UpPoint following interface CapitalPrintable
 	private int x, y; //instance variables
 	
 	UpPoint(int x, int y) { //constructor has two int parameters
@@ -9,6 +9,6 @@ public class UpPoint { //the class UpPoint
 	}
 	
 	public String toString() { //this method returns String type value
-		return "X : " + x + " Y : " + y; //returns upper case X and Y and values
+		return "x : " + x + " y : " + y; //returns lower case x and y and values
 	} 
 }
